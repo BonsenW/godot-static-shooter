9344425d921f77234e98cdd69544d9a9ba767ca5
Bonsen Wakjira <bonsenw@gmail.com>
2024-03-13 01:05:52 +1100

    Chaged from rigidbody2d to area2d for bullet, started working on health bar

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -1,8 +1,13 @@
-extends RigidBody2D
+extends Area2D
 
-var damage: int = 0
+var speed: int
+var damage: int
+var direction: Vector2
 
-func _on_body_entered(body: Node) -> void:
+func _process(delta: float) -> void:
+	position += direction * speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("hit"):
 		body.hit(damage)
 		queue_free()
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -2,12 +2,11 @@ extends Node2D
 
 func _on_player_attack_input_pressed(bullet_path: Resource, spawn_position: Vector2, damage: int, fire_power: int) -> void:
 	# Instantiate the bullet
-	var bullet := bullet_path.instantiate() as RigidBody2D
+	var bullet := bullet_path.instantiate() as Area2D
 	# Set properties of bullet
 	bullet.position = spawn_position
 	bullet.damage = damage
-	# Shoot bullet
-	print(fire_power)
-	bullet.apply_impulse(spawn_position.normalized() * fire_power)
+	bullet.direction = spawn_position.normalized()
+	bullet.speed = fire_power
 	# Add the bullet instance as a child of the current scene
 	add_child(bullet)
